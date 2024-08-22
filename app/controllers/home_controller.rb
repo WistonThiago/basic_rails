43b4a737e9842f2511998009236e12fa71351c33
Wiston Thiago <wthyago-2000@hotmail.com>
2024-08-22 19:23:19 -0300

    Creating the view for a new function (and the route) and calling instance variables.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -33,4 +33,13 @@ class HomeController < ApplicationController
     render html: message.html_safe
 
   end
+
+  def passdatatoviews
+    @message="<h1>Hello world from Rails 7</h1>"
+    @message+="<h2>Hello world from Rails 7</h2>"
+    @message+="<h3>Hello world from Rails 7</h3>"
+    @message+="<h4>Hello world from Rails 7</h4>"
+    @message+="<h5>Hello world from Rails 7</h5>"
+    @message+="<h6>Hello world from Rails 7</h6>"
+  end
 end
