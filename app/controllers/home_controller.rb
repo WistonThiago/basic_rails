be9825ad059bf2f07e9739cbed9b478c354c4ccf
Wiston Thiago <wthyago-2000@hotmail.com>
2024-08-22 18:54:17 -0300

    Now hello world with HTML.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    render plain: "Hello World from Rails 7"
+    #render plain: "Hello World from Rails 7"
+    render html: "<h1>Hello World from Rails 7 (HTML)</h1>".html_safe
   end
 end
