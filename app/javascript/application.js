b8655ef05ad9fd40b40bd20246b067c671c948a8
Wiston Thiago <wthyago-2000@hotmail.com>
2025-02-28 21:42:03 -0300

    Using jquery-rails to work with a checkbox event.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require jquery
 import "@hotwired/turbo-rails"
 import "controllers"
+
+$(document).on('turbo:load', function(e) {
+    hideShowAddress();
+});
+
+function hideShowAddress() {
+    $('#address_details').on('change', function() {
+        $('.address-details-div').toggleClass("d-none");
+
+        if ($("#lblShowHide").text() == "Hide Address Details")
+            $("#lblShowHide").text("Show Address Details")
+        else
+            $("#lblShowHide").text("Hide Address Details")
+    });
+}
